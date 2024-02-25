25d22ebef8b6263a33f1261c769a6e19312040e8
IsmaelRuizJurado <ismruijur@alum.us.es>
2024-02-25 18:45:27 +0100

    Tareas ismael iniciadas

---
+++ src/main/java/acme/entities/project/Project.java
@@ -0,0 +1,69 @@
+
+package acme.entities.project;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import acme.roles.Manager;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Project extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@NotNull
+	@Pattern(regexp = "[A-Z]{3}-[0-9]{4}", message = "{validation.project.code}")
+	private String				code;
+
+	@NotNull
+	@NotBlank
+	@Length(min = 1, max = 75)
+	private String				title;
+
+	@NotNull
+	@NotBlank
+	@Length(min = 1, max = 100)
+	private String				abstractt;
+
+	@NotNull
+	private boolean				indication;
+
+	@Valid
+	@NotNull
+	private Money				cost;
+
+	@URL
+	private String				link;
+
+	private boolean				draftMode;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Manager				manager;
+
+}
---
+++ src/main/java/acme/entities/user_stories/UserStory.java
@@ -0,0 +1,65 @@
+
+package acme.entities.user_stories;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import acme.entities.project.Project;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserStory extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotNull
+	@NotBlank
+	@Length(min = 1, max = 75)
+	private String				title;
+
+	@NotNull
+	@NotBlank
+	@Length(min = 1, max = 100)
+	private String				description;
+
+	@Valid
+	@NotNull
+	private Money				estimatedCostPerHour;
+
+	@NotNull
+	@NotBlank
+	@Length(min = 1, max = 100)
+	private String				acceptanceCriteria;
+
+	@Valid
+	@NotNull
+	private Priority			priority;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
+
+}
