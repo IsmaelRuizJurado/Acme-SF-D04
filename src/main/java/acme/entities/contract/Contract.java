81ce0b047fbc40f1590791fe14fe00248538ec8c
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2024-02-29 15:22:34 +0100

    contract and progressLogs sample added

--- src/main/java/acme/entities/contract/Contract.java
+++ src/main/java/acme/entities/contract/Contract.java
@@ -9,6 +9,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -57,6 +58,8 @@ public class Contract extends AbstractEntity {
 	@Length(min = 1, max = 100)
 	private String				goals;
 
+	@Min(0)
+	@NotNull
 	private Double				budget;
 
 	private boolean				draftMode;
