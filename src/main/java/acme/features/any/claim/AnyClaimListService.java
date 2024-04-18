a80e71fdc73d0acc454e16b5381a471267ec2e32
diemanane <diemanane@alum.us.es>
2024-04-18 16:16:50 +0200

    Grupal hecha pero no funciona

---
+++ src/main/java/acme/features/any/claim/AnyClaimListService.java
@@ -0,0 +1,45 @@
+
+package acme.features.any.claim;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Any;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.claim.Claim;
+
+@Service
+public class AnyClaimListService extends AbstractService<Any, Claim> {
+
+	@Autowired
+	protected AnyClaimRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Claim> objects;
+		objects = this.repository.findAllClaims();
+
+		super.getBuffer().addData(objects);
+	}
+
+	@Override
+	public void unbind(final Claim object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "instantiationMoment", "description", "department");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
